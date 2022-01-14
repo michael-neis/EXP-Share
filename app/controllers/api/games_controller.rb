83813295fec845a9444b214955328d0519eae2b2
Michael Neis <michael.neis12@gmail.com>
2022-01-14 15:54:58 -0600

    token test

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -22,8 +22,8 @@ class Api::GamesController < ApplicationController
 
             :headers => {
               "Content-Type" => "text/plain",
-              "Client-ID" => "abc",
-              "Authorization" => "abc"
+              "Client-ID" => "#{ENV['client_id_token']}",
+              "Authorization" => "#{ENV['authorization_bearer_token']}"
             },
             :body => "fields *, collection.name, collection.games.name, cover.url, platforms.name, platforms.platform_logo.url;  where id = #{game_id};"
           )
